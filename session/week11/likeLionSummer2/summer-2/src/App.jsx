2d2121045a6d27f52a61594d8cce851528bd1ba6
ZZZINU <nnjin987@gmail.com>
2023-07-06 17:33:41 +0900

    Create: week11 세션자료

---
+++ session/week11/likeLionSummer2/summer-2/src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import React from 'react';
+import FakeNews from './components/FakeNews';
+
+
+function App() {
+  return (
+    <>
+    <FakeNews></FakeNews>
+
+    </>
+  );
+}
+
+export default App;
---
+++ session/week11/likeLionSummer2/summer-2/src/components/useContext/ContextComponent.jsx
@@ -0,0 +1,13 @@
+// ContextComponent.jsx
+import React from 'react';
+import Child from './Child';
+
+
+export default function ContextComponent() {
+  return (
+    <div>
+      내 이름은....
+    <Child></Child>
+    </div>
+  );
+}
