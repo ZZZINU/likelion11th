b88693e7295b9de7bacd7ee0e8c1064e710d6dee
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-08-07 16:57:10 +0900

    Feat: 모달창 외부 클릭 시 사라지게 구현

--- homework/week14/FE_Netflix_Clone_Coding-main/src/components/MainPage/Row.jsx
+++ homework/week14/FE_Netflix_Clone_Coding-main/src/components/MainPage/Row.jsx
@@ -1,25 +1,25 @@
-import { useEffect, useState } from 'react'
-import axios from '../../api/axios'
-import MovieModal from '../MovieModal/MovieModal'
-import { RowContainer, RowPosters, RowPoster, RowTitle } from './Styled'
+import { useEffect, useState } from "react";
+import axios from "../../api/axios";
+import MovieModal from "../MovieModal/MovieModal";
+import { RowContainer, RowPosters, RowPoster, RowTitle } from "./Styled";
 
 export default function Row({ isLarge, title, id, fetchUrl }) {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
   // fetchUrl이 변화할 때마다 fetchMovieData를 실행
   useEffect(() => {
-    fetchMovieData()
-  }, [fetchUrl])
+    fetchMovieData();
+  }, [fetchUrl]);
 
   // 비동기 함수
   const fetchMovieData = async () => {
     // 받아온 fetchUrl로 GET 요청
-    const request = await axios.get(fetchUrl)
-    console.log(request)
+    const request = await axios.get(fetchUrl);
+    console.log(request);
 
-    setMovies(request.data.results)
-  }
-  console.log(movies)
+    setMovies(request.data.results);
+  };
+  console.log(movies);
 
   const [modalOpen, setModalOpen] = useState(false);
 
@@ -28,29 +28,29 @@ export default function Row({ isLarge, title, id, fetchUrl }) {
   const handleClick = (movie) => {
     setModalOpen(true);
     setMovieSelected(movie);
-  }
+  };
 
   return (
     <>
-    {modalOpen && (
-      <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
-    )}
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
       <RowContainer>
         <RowTitle>{title}</RowTitle>
         <RowPosters>
-          {movies.map(movie => (
+          {movies.map((movie) => (
             <RowPoster
               key={movie.id}
-              islarge={isLarge ? 'true' : 'false'}
+              islarge={isLarge ? "true" : "false"}
               src={`https://image.tmdb.org/t/p/original/${
                 isLarge ? movie.poster_path : movie.backdrop_path
               }`}
               alt={movie.name}
-              onClick={()=>handleClick(movie)}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </RowPosters>
       </RowContainer>
     </>
-  )
+  );
 }
